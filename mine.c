b56f71f06b8adcb223fec33b444ff69d78329df6
KOREAN139 <korean139@gmail.com>
2018-04-03 02:50:23 +0900

    Feat draw board
    Add next screen after difficulty selection from very first screen.
    
    It draws board with given (or pre-set) width & height, and waits
    for next input from user.
    
    This commit covers every case in custom mode that can be occurred
    with width and height, such as too big width (or height) for current
    screen has given.

--- mine.c
+++ mine.c
@@ -3,9 +3,11 @@
 #include <unistd.h>
 #include <ncurses.h>
 
-int main()
-{
-  int _W, _H, i, j, c, pos = 0;
+/* Current screen's width & height */
+int _W, _H;
+
+int startScreen(){
+  int i, c, pos = 0;
   char *title = "M I N E S W E E P E R";
   char *difficulty[] = {
     "E A S Y ( 9 X 9, 1 6 M I N E S )",
@@ -13,25 +15,6 @@ int main()
     "H A R D ( 3 0 X 1 6, 9 9 M I N E S )",
     "C U S T O M"
   };  
-
-  /* Start curse mode */
-  initscr();
-  cbreak();
-  noecho();
-  keypad(stdscr, TRUE);
-
-  /* Set cursor invisible */
-  curs_set(0);
-
-  /*
-  printw("Upper left corner           "); addch(ACS_ULCORNER); printw("\n");
-  printw("Lower left corner           "); addch(ACS_LLCORNER); printw("\n");
-  printw("Upper right corner          "); addch(ACS_URCORNER); printw("\n");
-  printw("Lower right corner          "); addch(ACS_LRCORNER); printw("\n");
-  */
-
-  /* Get current screen's width and height */
-  getmaxyx(stdscr, _H, _W);
 
   /* Print title */
   for(i = 0; title[i]; i++){
@@ -50,13 +33,13 @@ int main()
     refresh();
   }
 
-
   /* Print difficulties */
   for(i = 0; i < 4; i++){
     usleep(200000);
     mvprintw(_H/2 - 3 + i*2, _W/2 - 10, "%s", difficulty[i]);
     refresh();
   }
+
   mvprintw(_H/2 - 3 + pos*2, _W/2 - 12, ">");
   refresh();
 
@@ -70,6 +53,140 @@ int main()
     mvprintw(_H/2 - 3 + pos*2, _W/2 - 12, ">");
     refresh();
   }
+  
+  return pos;
+}
+
+void drawBoard(int width, int height){
+  int i, j;
+  int baseW, baseH;
+
+  /* Set base point at left upper corner of board */
+  baseW = _W/2 - width;
+  baseH = _H/2 - height;
+
+  /* Draw upper half */
+  usleep(100000);
+  move(baseH, baseW);
+  addch(ACS_ULCORNER);
+  refresh();
+
+  for(i = 1; i < 2*width + 1; i++){
+    for(j = 0; j <= i && j < 2*height + 1; j++){
+      move(baseH + j, baseW + i - j);
+      if(i & 1) addch(j & 1 ? ACS_VLINE : ACS_HLINE);
+      else{
+        if(j & 1) continue;
+        else if(j < 2*height) addch(j ? i > j ? ACS_PLUS : ACS_LTEE : ACS_TTEE);
+        else addch(ACS_BTEE);
+        if(i == j && j == 2*height) mvaddch(baseH + j, baseW, ACS_LLCORNER);
+      }
+    }
+    usleep(20000);
+    refresh();
+  }
+  
+  /* Draw lower half */
+  move(baseH, baseW + width*2);
+  addch(ACS_URCORNER);
+
+  for(i = 1; i < 2*height + 1; i++){
+    for(j = 0; j + i < height*2 + 1 && j < 2*width + 1; j++){
+      move(baseH + i + j, baseW + 2*width - j);
+      if(i & 1) addch(j & 1 ? ACS_HLINE : ACS_VLINE);
+      else{
+        if(j & 1) continue;
+        else if(j < 2*width) addch(j ? i + j < 2*height ? ACS_PLUS : ACS_BTEE : ACS_RTEE);
+        else addch(ACS_LTEE);
+        if(i + j == 2*height && j == 2*width) mvaddch(baseH + i + j, baseW, ACS_LLCORNER);
+        if(i == 2*height) mvaddch(baseH + i, baseW + 2*width, ACS_LRCORNER);
+      }
+    }
+    usleep(20000);
+    refresh();
+  }
+
+  getch();
+}
+
+int main(){
+  int op;
+  int width, height, mine;
+
+  /* Start curse mode */
+  initscr();
+
+  /* Make characters typed immediately available to the program */
+  cbreak();
+
+  /* Do not echo characters typed */
+  noecho();
+  
+  /* Let getch() can get function key */
+  keypad(stdscr, TRUE);
+
+  /* Set cursor invisible */
+  curs_set(0);
+
+  /* Get current screen's width and height */
+  getmaxyx(stdscr, _H, _W);
+
+  /* Draw start screen, gets difficulty */
+  op = startScreen();
+
+  /* Set width, height with given option(difficulty) */
+  switch(op){
+    case 0: 
+      width = 9; 
+      height = 9; 
+      mine = 16;
+      break;
+    case 1: 
+      width = 16; 
+      height = 16;
+      mine = 40;
+      break;
+    case 2: 
+      width = 30; 
+      height = 16;
+      mine = 99;
+      break;
+    case 3:
+      clear();
+      echo();
+      curs_set(1);
+      do{
+        clear();
+        mvprintw(_H/2 - 5, _W/2 - 10, "Set your custom game :)");
+        mvprintw(_H/2 - 3, _W/2 - 10, "WIDTH (MAX %d) : ", _W/2 - 1);
+        refresh();
+        scanw("%d", &width);
+      } while(width > _W/2 - 1);
+      do{
+        clear();
+        mvprintw(_H/2 - 5, _W/2 - 10, "Set your custom game :)");
+        mvprintw(_H/2 - 3, _W/2 - 10, "WIDTH (MAX %d) : %d", _W/2 - 1, width);
+        mvprintw(_H/2 - 1, _W/2 - 10, "HEIGHT (MAX %d) : ", _H/2 - 1);
+        refresh();
+        scanw("%d", &height);
+      } while(height > _H/2 - 1);
+      do{
+        clear();
+        mvprintw(_H/2 - 5, _W/2 - 10, "Set your custom game :)");
+        mvprintw(_H/2 - 3, _W/2 - 10, "WIDTH (MAX %d) : %d", _W/2 - 1, width);
+        mvprintw(_H/2 - 1, _W/2 - 10, "HEIGHT (MAX %d) : %d", _H/2 - 1, height);
+        mvprintw(_H/2 + 1, _W/2 - 10, "MINES (MAX %d) : ", width*height - 1);
+        refresh();
+        scanw("%d", &mine);
+      } while(mine > width*height - 1);
+      noecho();
+      curs_set(0);
+  }
+  clear();
+  refresh();
+
+  /* Draw board */
+  drawBoard(width, height);
 
   endwin();
   return 0;
